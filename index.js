89ccc63f4dfad176ac801a75fd54e36fb0f65885
marwa <mds08@mail.aub.edu>
2022-08-07 23:02:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 'sid'
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'it is bob!'
+}
